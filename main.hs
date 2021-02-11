a3e941fc4c461701a28b843b4cf4603f5f4a4f79
Sander Karsten <s.karsten@student.science.ru.nl>
2021-02-11 12:32:02 +0100

    Changes together on feb 11, tried idliteral and made parser for op and type and changed previous types to expr

--- main.hs
+++ main.hs
@@ -18,43 +18,59 @@ data FunType = P
 
 data FTypes = Undef
 
-data SPLType = TypeBasic BasicType
-          | TypeTuple (SPLType, SPLType)
-          | TypeArray [SPLType]
-          deriving (Show, Eq)
+data SPLType 
+  = TypeBasic BasicType
+  | TupleType (SPLType, SPLType)
+  | ArrayType SPLType
+  deriving (Show, Eq)
           
 data BasicType
-  = BasicInt Integer
-  | BasicBool Bool
-  | BasicChar Char
+  = BasicInt
+  | BasicBool
+  | BasicChar
   deriving (Show, Eq)
 
-data Fargs = Hallo000
+data Fargs = Hallo
 
-data Stmt = Statement
+data Stmt = IfStmt (Exp, [Stmt]) [(Exp, [Stmt])]
+            | WhileStmt Exp Stmt
 
-data Exp = ExpId Id Field
-         | ExpInt Integer
-         | ExpChar Char
-         | ExpBool Bool
-         | BracketExp Exp
-         | ExpOp2 Exp Op2 Exp
-         | ExpOp1 Exp
-         | ExpFunCall FunCall
+data Exp 
+  = ExpId String Field
+  | ExpInt Integer
+  | ExpChar Char
+  | ExpBool Bool
+  | ExpBracket Exp
+  | ExpOp2 Exp Op2 Exp
+  | ExpOp1 Op1 Exp
+  | ExpFunCall FunCall
+  | ExpArray [Exp]
+  | ExpTuple (Exp, Exp)
+  deriving (Show, Eq)
+
          -- | ExpArray 
          -- | ExpTuple (Exp, Exp)
-
+        
 -- TODO: Types kloppen niet helemaal meer met beschrijving, zorg dat het in EXp komt te staan
+data Op1 = Not
+         | Neg
+        deriving (Show, Eq)
+        
+data Op2 = Plus|Min|Mult|Div|Mod|Eq|Le|Ge|Leq|Geq|Neq|And|Or|Con
+         deriving (Show, Eq)
 
-data Op2 = Empty 
-data Id = Doei 
+-- data Id = Doei
+--         deriving (Show, Eq)
+
 data Field = Fjeld
-data FunCall = CallFun
+            deriving (Show, Eq)
 
+data FunCall = CallFun
+        deriving (Show, Eq)
 
 
 newtype Code = Code [(Char, Int, Int)]
-  deriving (Show)
+  deriving (Show, Eq)
 
 -- ======================================== Parser Error ========================================
 
@@ -143,41 +159,125 @@ charLiteral = Parser f
       case x of
         (ch, line, col) -> Right (ch, Code xs)
 
--- ======================================== BasicType ========================================
 
-basicChar :: Parser BasicType
-basicChar = BasicChar <$> (charP '\'' *> charLiteral <* charP '\'')
+-- idLiteral :: Parser String
+-- idLiteral = Parser f
+--   where 
+--     f (Code (x:xs)) = 
+--       case x of (ch, line, col) -> Left (ch, Code xs)
 
-basicBool :: Parser BasicType
-basicBool = (BasicBool True <$ stringP "true") <|>
-            (BasicBool False <$ stringP "false")
+-- idLiteral :: Parser String
+-- idLiteral = \input -> 
+--   case run (spanP (\(c,line,col) -> isAlpha c)) input of
+--     Right (x, y) -> Error 0 0 "Can't match with an ID"
+    -- Right ([], ((Code (c, line, col)):ys)) -> Error line col "Can't match with an ID"
 
--- TODO: Implement notNull
--- It works but not when there are no numbers the same was the case for the tutorial
-basicInt :: Parser BasicType
-basicInt = f <$> notEmpty (spanP (\(a,b,c) -> isDigit a))
-  where f x = BasicInt(read x)
 
-basicType :: Parser BasicType
-basicType = basicChar <|> basicInt <|>  basicBool
+    -- Right ((x:xs), code) -> 
+    --   let Right (s, code') = run (spanP (\(c,line,col) -> isAlphaNum c || c == '_')) code
+    --   in Right ((x:xs)++s, code')
+  
+-- let Right s (Code x:xs) = run (spanP (\(c,line,col) -> isAlpha c) input 
+-- in 
+-- <*> spanP (\(c,line,col) -> isAlphaNum c || c == '_')
+-- idLiteral = ((++) . concat <$> some(spanP (\(c,line,col) -> isAlpha c))) <*> (concat <$> many(spanP (\(c,line,col) -> isAlphaNum c)))
 
 -- ======================================== SPLType ========================================
 
--- This should become exp and BasicType should be implemented differently
-
-typeTuple :: Parser SPLType
-typeTuple = TypeTuple <$> (charP '(' *> ws *> elems <* ws <* charP ')')
+tupleType :: Parser SPLType
+tupleType = TupleType <$> (charP '(' *> ws *> elems <* ws <* charP ')')
   where elems = (\first _ second -> (first, second)) <$> splType <*> (ws *> charP ',' <* ws) <*> splType
-    
---We assumed that array elements in SPL are seperated by a comma
-typeArray :: Parser SPLType
-typeArray = TypeArray <$> (charP '[' *> ws *> elems <* ws <* charP ']')
-  where elems = sepBy (ws *> charP ',' <* ws ) splType
+
+arrayType :: Parser SPLType
+arrayType = ArrayType <$> (charP '[' *> ws *> splType <* ws <* charP ']')
 
 splType :: Parser SPLType
-splType = typeTuple <|> typeArray <|> TypeBasic <$> basicType
+splType =  arrayType <|> tupleType <|> TypeBasic <$> basicType
 
+-- ======================================== BasicType ========================================
+
+basicInt :: Parser BasicType
+basicInt = BasicInt <$ stringP "Int"
+
+basicBool :: Parser BasicType
+basicBool = BasicBool <$ stringP "Bool"
+
+basicChar :: Parser BasicType
+basicChar = BasicChar <$ stringP "Char"
+
+basicType :: Parser BasicType
+basicType = basicInt <|> basicBool <|> basicChar
+
+-- ======================================== Exp ========================================
+
+expChar :: Parser Exp
+expChar = ExpChar <$> (charP '\'' *> charLiteral <* charP '\'')
+
+expBool :: Parser Exp
+expBool = (ExpBool True <$ stringP "True") <|>
+            (ExpBool False <$ stringP "False")
+
+-- TODO: Implement notNull
+-- It works but not when there are no numbers the same was the case for the tutorial
+expInt :: Parser Exp
+expInt = f <$> notEmpty (spanP (\(a,b,c) -> isDigit a))
+  where f x = ExpInt(read x)
+
+expTuple :: Parser Exp
+expTuple = ExpTuple <$> (charP '(' *> ws *> elems <* ws <* charP ')')
+  where elems = (\first _ second -> (first, second)) <$> parserExp <*> (ws *> charP ',' <* ws) <*> parserExp
+
+--We assumed that array elements in SPL are seperated by a comma
+expArray :: Parser Exp
+expArray = ExpArray <$> (charP '[' *> ws *> elems <* ws <* charP ']')
+  where elems = sepBy (ws *> charP ',' <* ws ) parserExp
+
+expOp1 :: Parser Exp
+expOp1 = ExpOp1 <$> op1 <*> parserExp
+
+expOp2 :: Parser Exp
+expOp2 = ExpOp2 <$> parserExp <*> op2 <*> parserExp
+
+expBracket :: Parser Exp
+expBracket = ExpBracket <$> (charP '(' *> ws *> parserExp <* ws <* charP ')')
+ 
+parserExp :: Parser Exp
+parserExp = expChar <|> expInt <|> expBool <|> expArray <|> expTuple <|> expOp1 <|> expOp2
+
+-- ======================================== Stmt ============================================
+--data Stmt = IfStmt (Exp, Stmt) [(Exp, Stmt)]
+            -- | WhileStmt Exp Stmt
+
+-- ifStmt :: Parser Stmt
+-- ifStmt = IfStmt <$> (stringP "if" *> ws *> charP '(' *> ws *> parserExp <* ws <* charP ')' <*> 
+--                     charP '{' *> ws *> stmts <* ws <* charP '}') <*> []
+--                     where stmts = sepBy (ws *> charP ';' <* ws) stmt
+
+-- stmt :: Parser Stmt
+-- stmt = ifStmt
+
+-- ======================================== Op ==============================================
+op1 :: Parser Op1
+op1 = (Not <$ charP '!') <|> (Neg <$ charP '-')
+
+op2 :: Parser Op2
+op2 = (Plus <$ charP '+') <|>
+      (Min <$ charP '-') <|>
+      (Mult <$ charP '*') <|>
+      (Div <$ charP '/') <|>
+      (Mod <$ charP '%') <|>
+      (Eq <$ stringP "==") <|>
+      (Le <$ charP '<') <|>
+      (Ge <$ charP '>') <|>
+      (Leq <$ stringP "<=") <|>
+      (Geq <$ stringP ">=") <|>
+      (Neq <$ stringP "!=") <|>
+      (And <$ stringP "&&") <|>
+      (Or <$ stringP "||") <|>
+      (Con <$ charP ':')
 -- ==========================================================================================
+  
+
 
 main :: IO ()
 main = undefined
