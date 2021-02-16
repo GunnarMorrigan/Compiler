0b0be5b0f9246771469898c469e6895c089937ff
gunnarpieter <13799935+gunnarpieter@users.noreply.github.com>
2021-02-16 22:38:25 +0100

    Altering grammar to avoid left rec and fix precedence

--- Grammar.hs
+++ Grammar.hs
@@ -32,23 +32,40 @@ data Stmt = StmtIf Exp [Stmt] (Maybe [Stmt])
           | StmtReturn (Maybe Exp)
           deriving (Show, Eq)
 
--- data Exp 
---   = ExpId String Field
---   | ExpInt Integer
---   | ExpChar Char
---   | ExpBool Bool
---   | ExpBracket Exp
---   | ExpOp2 Exp Op2 Exp
---   | ExpOp1 Op1 Exp
---   | ExpFunCall FunCall
---   | ExpArray [Exp]
---   | ExpTuple (Exp, Exp)
---   deriving (Show, Eq)
-
 data HigherExp
-    = OrExp HigherExp OpPre12 AndExp 
-    | NextOrExp AndExp
+    = ExpOp1 Op1 HigherExp
+    | ExpOr OrExp 
+    | ExpOrRec OrExp HigherExp
+  deriving(Show, Eq)
+
+data OrExp 
+    = ExpAnd AndExp
+    | ExpAndRec AndExp OrExp
     deriving(Show, Eq)
+
+data AndExp 
+    = ExpConst ConstExp 
+    | ExpConstRec ConstExp AndExp
+    deriving (Show, Eq)
+
+data ConstExp 
+    = ExpComp CompExp 
+    | ExpCompRec CompExp OpComp ConstExp
+    deriving (Show, Eq)
+
+-- Le, Ge, Leq, Geq, Eq and Neq
+data CompExp
+    = ExpPlusMin PlusMinExp
+    | ExpPlusMinRec PlusMinExp CompExp
+    deriving (Show, Eq)
+
+data PlusMinExp
+    = PlusMinExp MultDivModExp [(OpPre4, MultDivModExp)]
+    deriving (Show, Eq)
+
+data MultDivModExp
+    = MultDivModExp Exp [(OpPre3, Exp)]
+    deriving (Show, Eq)
 
 data Exp 
   = ExpId String Field
@@ -56,37 +73,10 @@ data Exp
   | ExpChar Char
   | ExpBool Bool
   | ExpBracket Exp
-  | ExpOp1 Op1 Exp
   | ExpFunCall FunCall
-  | ExpArray [Exp]
+  | ExpEmptyList
   | ExpTuple (Exp, Exp)
   deriving (Show, Eq)
-      
-data AndExp 
-    = AndExp AndExp OpPre11 CompExp
-    | NextAndExp CompExp
-    deriving (Show, Eq)
-
--- Le, Ge, Leq, Geq, Eq and Neq
-data CompExp
-    = LeGeExp CompExp OpPre6 PlusMinExp
-    | NextLeGeExp PlusMinExp
-    deriving (Show, Eq)
-
-data PlusMinExp 
-    = PlusMinExp MultDivModExp [(OpPre4, PlusMinExp)]
-    | NextPlusMinExp  MultDivModExp
-    deriving (Show, Eq)
-
-data MultDivModExp
-    = MultDivModExp ConExp [(OpPre3, MultDivModExp)]
-    | NextMultDivModExp ConExp
-    deriving (Show, Eq)
-
-data ConExp 
-    = ConExp ConExp OpCon Exp
-    | RecExp HigherExp
-    deriving (Show, Eq)
 
 data OpCon = Con deriving (Show, Eq)
 data Op1 = Neg | Not deriving (Show, Eq)
@@ -94,10 +84,11 @@ data Op1 = Neg | Not deriving (Show, Eq)
 -- ==== Op2 ====
 data OpPre3 = Mult|Div|Mod deriving (Show, Eq)
 data OpPre4 = Plus|Min deriving (Show, Eq)
-data OpPre6 = Le|Ge|Leq|Geq|Eq|Neq deriving (Show, Eq)
+data OpComp = Le|Ge|Leq|Geq|Eq|Neq deriving (Show, Eq)
 
-data OpPre11 = And deriving (Show, Eq)
-data OpPre12 = Or deriving (Show, Eq)
+-- Not needed
+-- data OpPre11 = And deriving (Show, Eq)
+-- data OpPre12 = Or deriving (Show, Eq)
 
 -- ========
 
