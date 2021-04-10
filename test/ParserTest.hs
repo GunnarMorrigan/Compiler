54c08a3cb0c31ead26c9c78c57fd5a0395874f58
gunnarpieter <13799935+gunnarpieter@users.noreply.github.com>
2021-04-10 12:35:34 +0200

    Just some changes

--- test/ParserTest.hs
+++ test/ParserTest.hs
@@ -3,7 +3,7 @@ import Test.HUnit
 
 import Control.Monad
 import Data.Map as Map
-import System.Directory
+import System.Directory ( getCurrentDirectory )
 
 import Lexer
 import Parser
@@ -11,12 +11,14 @@ import AST
 
 -- main :: IO()
 parserTest1 = TestCase $ do
-       file <- readFile  "test/AutoTestSpl/test1.spl"
-       case tokeniseAndParse mainSegments file of 
-              Right (x, _) -> do
-                    assertEqual "parser test 1" file (pp x)
-              Left x -> do
-                    assertFailure $ show x ++ "\n" ++ showPlaceOfError file x
+      -- path <- getCurrentDirectory
+      -- print path
+      file <- readFile  "./test/AutoTestSPL/test1.spl"
+      case tokeniseAndParse mainSegments file of 
+            Right (x, _) -> do
+                  assertEqual "parser test 1" file (pp x)
+            Left x -> do
+                  assertFailure $ show x ++ "\n" ++ showPlaceOfError file x
 
 
 
