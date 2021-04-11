95f5dd0a272044361c7b00b7a8ff9cb060efa87b
gunnarpieter <13799935+gunnarpieter@users.noreply.github.com>
2021-04-11 12:54:45 +0200

    added insert scheme into TypeEnv and changed getLineNum getColNum to return Int instead of string

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -6,6 +6,7 @@ import Control.Applicative
 import Data.Char
 
 data Error = Error Int Int String
+  deriving (Show)
 
 instance Eq Error where
   (==) (Error line col m) (Error line' col' m') =  line == line' && col == col'
@@ -13,9 +14,9 @@ instance Eq Error where
 instance Ord Error where
   (Error line col m) `compare` (Error line' col' m') = if line == line' then compare col col' else compare line line'
 
-instance Show Error where
-  show (Error (-1) (-1) message) = message
-  show (Error line col message) = message ++ ", on line: " ++show line ++ " and character: " ++ show col 
+-- instance Show Error where
+--   show (Error (-1) (-1) message) = message
+--   show (Error line col message) = message ++ ", on line: " ++show line ++ " and character: " ++ show col 
 
 instance Alternative (Either Error) where
   empty = Left $ Error 0 0 ""
@@ -38,6 +39,7 @@ data Token
   | IdToken String
   | IfToken| ElseToken| WhileToken
   deriving (Eq)
+  
 instance Show Token where
   show VarToken = "var"
   show (IntToken x) = show x
--- test/TITest.hs
+++ test/TITest.hs
@@ -4,8 +4,10 @@ import Test.HUnit
 import Control.Monad
 import Data.Map as Map
 
-import TI
 import AST
+import Lexer
+import Parser
+import TI
 
 -- ====== Tests regarding getType ======
 getTypeTest1 = TestCase (assertEq "getType test 1" expected f)
@@ -26,12 +28,12 @@ getTypeTest2 = TestCase (assertEq "getType test 2" expected f)
 
 getTypeTests = [TestLabel "getTypeTest1" getTypeTest1, TestLabel "getTypeTest2" getTypeTest2]
 
--- ====== Tests regarding tiExp ======
 
+-- ====== Tests regarding tiExp ======
 expTest1 = TestCase (assertEqual "tiExp test 1" expected f)
     where
         f = let (res, s) = runTI (tiExp (TypeEnv Map.empty) (ExpId (idLocCreator "hoi") (Field [Head, Second, Head]))) in res
-        expected = Left "id: 'hoi', referenced on Line -1 and, Col -1, has not been defined yet: (i.e. reference before declaration)"
+        expected = Left $ Error (-1) (-1) "id: 'hoi', referenced on Line -1 and, Col -1, has not been defined yet: (i.e. reference before declaration)"
 
 expTest2 = TestCase (assertEqual "tiExp test 2" expected f)
     where
@@ -58,13 +60,98 @@ expTest5 = TestCase (assertEqual "tiExp test 5" expected f)
             in res
         expected = Right (empty,TypeBasic BasicInt)
 
-
-
 expTest6 = TestCase (assertEqual "tiExp test 6" expected f)
     where
         f = let (res, s) = runTI (tiExp (TypeEnv (Map.fromList [(idLocCreator "hoi", Scheme [] (TypeBasic BasicBool) )])) (ExpOp1 Not $ ExpId (idLocCreator "hoi") (Field [])))
             in res
         expected = Right (empty,TypeBasic BasicBool)
+
+
+
+
+-- ====== Tests regarding tiStmts ======
+
+-- ("flip" :: ID, Scheme [] (TypeBasic BasicBool)),
+
+env' = 
+    [
+    ("first" :: ID, Scheme [] (IdType (idLocCreator "x") Nothing)),
+    ("sec" :: ID, Scheme [] (IdType (idLocCreator "y") Nothing)),
+    ("tuple" :: ID, Scheme [] (IdType (idLocCreator "z") Nothing))
+    ]
+
+env'' = 
+    [("tuple" :: ID, Scheme [] (IdType (idLocCreator "z") Nothing))
+    ]
+
+
+-- stmtsTest1 :: [Stmt] -> IO()
+-- stmtsTest1 stmts = let (res, s) = runTI (tiStmtTest (TypeEnv (Map.fromList env'')) (head stmts) )
+--     in case res of
+--          Left err ->  putStrLn $ "error: " ++ err
+--          Right (subst, t1, t2, env) ->  putStrLn $ show subst 
+--             ++ "\n\n" ++ show t1 
+--             ++ "\n\n" ++ show t2
+--             ++ "\n\n" ++ show env
+
+
+-- stmtsTest2 :: [Stmt] -> IO()
+-- stmtsTest2 stmts = let (res, s) = runTI (tiStmtsTest (TypeEnv (Map.fromList env')) stmts )
+--     in case res of
+--          Left err ->  putStrLn $ "error: " ++ err
+--          Right (subst, t, env) ->  putStrLn $ show subst ++ "\n\n" ++ show t ++ "\n\n" ++ show env
+
+-- runStmtsTest :: ([Stmt] -> IO()) -> String -> IO()
+-- runStmtsTest test filename = do
+--        file <- readFile $ splFilePath++filename
+--        case tokeniseAndParse (some stmt) file of 
+--               Right (x, _) -> do
+--                     print x
+--                     putStr "\n\n"
+--                     test x
+--               Left x -> do print x
+
+-- ====== Tests regarding tiFunDecl ======
+fundecl = FunDecl (idLocCreator "swap") [idLocCreator "tuple"] Nothing [] [StmtDeclareVar (idLocCreator "tuple") (Field [First]) (ExpId (idLocCreator "tuple") (Field [First])),StmtDeclareVar (idLocCreator "tuple") (Field [Second]) (ExpId (idLocCreator "tuple") (Field [Second])),StmtReturn (Just (ExpId (idLocCreator "tuple") (Field [])))]
+fundecl' = FunDecl (idLocCreator "swap") [idLocCreator "tuple"] Nothing [] [StmtDeclareVar (idLocCreator "tuple") (Field [First]) (ExpId (idLocCreator "tuple") (Field [Second])),StmtDeclareVar (idLocCreator "tuple") (Field [Second]) (ExpId (idLocCreator "tuple") (Field [First]))]
+fundecl'' = FunDecl (idLocCreator "swap") [idLocCreator "tuple"] Nothing [VarDeclVar (idLocCreator "tmp") (ExpId (idLocCreator "tuple") (Field [First]))] [StmtDeclareVar (idLocCreator "tuple") (Field [First]) (ExpId (idLocCreator "tuple") (Field [Second])),StmtDeclareVar (idLocCreator "tuple") (Field [Second]) (ExpId (idLocCreator "tmp") (Field []))]
+
+fundecl''' = FunDecl (idLocCreator "swap") [idLocCreator "tuple"] Nothing [VarDeclVar (idLocCreator "tmp") (ExpId (idLocCreator "tuple") (Field [First]))] [StmtDeclareVar (idLocCreator "tuple") (Field [First]) (ExpId (idLocCreator "tuple") (Field [Second])),StmtDeclareVar (idLocCreator "tuple") (Field [Second]) (ExpId (idLocCreator "tmp") (Field [])),StmtReturn (Just (ExpId (idLocCreator "tuple") (Field [])))]
+
+funDeclTest1 ::  IO()
+funDeclTest1 = let (res, s) = runTI (tiFunDecl (TypeEnv Map.empty) fundecl''' )
+    in case res of
+         Left err ->  putStrLn $ "error: " ++ show err
+         Right (TypeEnv env) -> print env
+
+
+
+-- ====== Tests with full SPL code ======
+
+-- tiTest1 = TestCase $ do
+--       -- path <- getCurrentDirectory
+--       -- print path
+--       file <- readFile  "./test/AutoTestSPL/test1.spl"
+--       case tokeniseAndParse mainSegments file of 
+--             Right (x, _) -> do
+--                   assertEqual "ti test 1" file (pp x)
+--             Left x -> do
+--                   assertFailure $ show x ++ "\n" ++ showPlaceOfError file x
+
+
+
+
+showPlaceOfError :: String -> Error -> String
+showPlaceOfError code (Error line col msg) =
+    lines code !! (line -1) ++ "\n"
+    ++ replicate (col-1) ' ' ++ "^"
+
+
+
+
+
+
+
 
 tiExpTests = 
     [TestLabel "expTest1" expTest1,
