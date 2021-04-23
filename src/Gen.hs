a725de371998ef67002a41a66703a74499f9a4d2
gunnarpieter <13799935+gunnarpieter@users.noreply.github.com>
2021-04-24 01:13:47 +0200

    Globals on the heap

--- src/Gen.hs
+++ src/Gen.hs
@@ -9,11 +9,19 @@ import TI
 import Data.Char
 import Data.List ( intercalate )
 import Data.Map as Map
+import Data.Bifunctor as BI
+
+data Mem =
+    H Int |
+    L Int
+    deriving (Show, Eq)
+
+data Scope = Global | Local
 
 class GenCode a where
     gen :: a -> [String] -> GenEnv -> [String]
 
-type GenEnv = Map IDLoc Int
+type GenEnv = Map IDLoc Mem
 
 instance GenCode a => GenCode [a] where
     gen [] c env = c
@@ -23,31 +31,39 @@ instance GenCode SPL where
     gen (SPL decls) c env = "bra main":gen decls c env
 
 instance GenCode Decl where
-    gen (VarMain varDecl) c = gen varDecl c 
-    gen (FuncMain funDecl) c = gen funDecl c 
+    gen (VarMain varDecl) c = genVarDecl varDecl c Global
+    gen (FuncMain funDecl) c = gen funDecl c
 
-instance GenCode VarDecl where
-    gen (VarDeclType t i e) c env = case Map.lookup i env of
-        Just l -> gen e (("stl "++ show l):c) env
-        Nothing -> error ("Variable " ++ show i ++ " unkown in generator " ++ showLoc i)
-    gen (VarDeclVar _ _) c env = undefined
+genVarDecls :: [VarDecl] -> [String] -> Scope -> Map IDLoc Mem -> [String]
+genVarDecls [] c _ _ = c
+genVarDecls (x:xs) c s env = genVarDecl x (genVarDecls xs c s env) s env
+
+-- instance GenCode VarDecl where
+genVarDecl :: VarDecl -> [String] -> Scope -> Map IDLoc Mem -> [String]
+genVarDecl (VarDeclType t i e) c Global env = case Map.lookup i env of
+    Just h -> gen e (sth h:c) env
+    Nothing -> error ("Variable " ++ show i ++ " unkown in generator " ++ showLoc i)
+genVarDecl (VarDeclType t i e) c Local env = case Map.lookup i env of
+    Just l -> gen e (stl l:c) env
+    Nothing -> error ("Variable " ++ show i ++ " unkown in generator " ++ showLoc i)
+genVarDecl (VarDeclVar _ _) c _ env = undefined
 
 instance GenCode FunDecl where
-    gen (FunDecl (ID "main" _) [] (Just fType) vDecls stmts) c _ = do
-        let env' = constructEnv [] vDecls
+    gen (FunDecl (ID "main" _) [] (Just fType) vDecls stmts) c env = do
+        let env' = constructEnv env [] vDecls 
         case length vDecls of
-            0 -> let x = gen stmts (["trap 0","halt","; THE END debug:"] ++ c) env' in 
+            0 -> let x = gen stmts (["trap 0","halt","; THE END debug:"] ++ c) env' in
                 ("main : "++ head x) : tail x
-            l -> ("main : link " ++ show l): gen vDecls (gen stmts (["trap 0","halt","; THE END debug:"] ++ c) env') env'
-    gen (FunDecl (ID name _) args (Just fType) vDecls stmts) c _ = do
-        let env' = constructEnv args vDecls
+            l -> ("main : link " ++ show l): genVarDecls vDecls (gen stmts (["trap 0","halt","; THE END debug:"] ++ c) env') Local env'
+    gen (FunDecl (ID name _) args (Just fType) vDecls stmts) c env = do
+        let env' = constructEnv env args vDecls
         case length vDecls of
             0 -> (name ++ ": link 0"): gen stmts c env'
-            l -> (name ++ ": link " ++ show l): gen vDecls (gen stmts c env') env'
+            l -> (name ++ ": link " ++ show l): genVarDecls vDecls (gen stmts c env') Local env'
 
 instance GenCode Stmt where
     gen (StmtIf e stmts els loc) c env = undefined
-    gen (StmtWhile e stmts loc) c env = undefined 
+    gen (StmtWhile e stmts loc) c env = undefined
     gen (StmtDeclareVar (ID name loc) field exp) c env = undefined
     -- MAKE USE OF THE fType in funCall for overloading
     gen (StmtFuncCall (FunCall id args Nothing) loc) c env = undefined
@@ -59,20 +75,21 @@ instance GenCode Stmt where
         Nothing -> "unlink":"ret":c
         Just e -> gen e ("str RR":"unlink":"ret": c) env
 
-isVoidFun :: SPLType -> Bool 
-isVoidFun x = last (getArgsTypes x) `eqType` Void defaultLoc 
+isVoidFun :: SPLType -> Bool
+isVoidFun x = last (getArgsTypes x) `eqType` Void defaultLoc
 
-constructEnv :: [IDLoc] -> [VarDecl] -> GenEnv
-constructEnv xs ys = fromList $ args ++ zip decls [1..]
-    where 
-        args = zip xs [(negate (length xs)-1 )..] 
+constructEnv :: GenEnv -> [IDLoc] -> [VarDecl] -> GenEnv
+constructEnv env xs ys = fromList (Prelude.map (BI.second L) (args ++ zip decls [1..])) `union` env
+    where
+        args = zip xs [(negate (length xs)-1 )..]
         decls = Prelude.map (\(VarDeclType t id e) -> id) ys
 
 -- genExp :: Exp -> [String] -> Map IDLoc Int -> [String]
 
 instance GenCode Exp where
     gen (ExpId id field) c env = case Map.lookup id env of
-        Just x -> ldl x : c
+        Just (L x) -> ldl x : c
+        Just (H x) -> ldh x : c
         Nothing -> error ("Variable " ++ show id ++ " unkown in generator " ++ showLoc id)
     gen (ExpInt i _) c env = ldc i:c
     gen (ExpBool b loc) c env= ldc (if b then -1 else 0  ):c
@@ -88,7 +105,7 @@ instance GenCode Op2Typed where
     gen (Op2 Min _) c _ = "sub":c
     gen (Op2 Mult _) c _= "mul":c
     gen (Op2 Div _) c _= "div":c
-    gen (Op2 Mod _) c _= "mod":c 
+    gen (Op2 Mod _) c _= "mod":c
     -- Should be extended for other types of comparison
     gen (Op2 Le (Just opType)) c _= "lt":c
     gen (Op2 Ge (Just opType)) c _= "gt":c
@@ -96,28 +113,39 @@ instance GenCode Op2Typed where
     gen (Op2 Geq (Just opType)) c _= "ge":c
     gen (Op2 Eq (Just opType)) c _= "eq":c
     gen (Op2 Neq (Just opType)) c _= "ne":c
-    
+
     gen (Op2 And _) c _= "and":c
     gen (Op2 Or _) c _= "or":c
 
     gen (Op2 Con (Just opType)) c _= undefined
-        
+
 -- Load Local. Pushes a value relative to the markpointer.
-ldl :: Int -> String 
+ldl :: Int -> String
 ldl x = "ldl " ++ show x
+
+stl :: Mem -> String
+stl (L x) = "stl " ++ show x
 
 -- Load Constant. Pushes the inline constant on the stack.
 ldc :: Int -> String
 ldc x = "ldc " ++ show x
 
+-- Adjust Stack. Adjusts the stackpointer with fixed amount.
 ajs :: Int -> String
 ajs x = "ajs " ++ show x
+
+-- Load from Heap. Pushes a value pointed to by the value at the top of the stack. The pointer value is offset by a constant offset.
+ldh :: Int -> String
+ldh x = "ldl " ++ show x
+
+sth :: Mem -> String 
+sth (H x) = "sth " ++ show x
 
 
 mainGen filename = do
       file <- readFile  ("../SPL_test_code/" ++ filename)
-      case tokeniseAndParse mainSegments file >>= (mutRec . fst) >>= typeInference of 
+      case tokeniseAndParse mainSegments file >>= (mutRec . fst) >>= typeInference of
             Right(_, _, spl) -> do
-                let output = intercalate "\n" $ gen spl ["\n"] empty 
+                let output = intercalate "\n" $ gen spl ["\n"] empty
                 writeFile "../generated_ssm/gen.ssm" output
             Left x -> putStr $ show x ++ "\n" ++ showPlaceOfError file x
